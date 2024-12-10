6d0e8c0747109bf12c84af77642f187035475810
Paulopal-hue <olaiyapaul25@gmail.com>
2024-12-10 02:57:58 +0100

    furniture web
    furniture sales websites

---
+++ java.js
@@ -0,0 +1,22 @@
+
+
+
+
+  
+//   let images = ["image/mag1.jpg", "image/mag2.jpg", "image/mag3.jpg"]; // Array of image URLs
+
+//   let currentImageIndex = 0;
+
+
+
+//   function changeBackgroundImage() {
+
+//     document.body.style.backgroundImage = `url(${images[currentImageIndex]})`;
+
+//     currentImageIndex = (currentImageIndex + 1) % images.length;
+
+//   }
+
+
+
+//   setInterval(changeBackgroundImage, 1000); // Change image every 5 seconds
